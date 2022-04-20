90bca9d0d1b6501fad45352d9eef7eb891fa30e1
vivek <vivektej2299@gmail.com>
2022-04-20 15:20:33 +0530

    Add contest time check functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import alert from '../methods/alert'
 const Card = React.lazy(()=> import('./Card'))
 
 
-const Home = ({setprogress}) => {
+const   Home = ({setprogress}) => {
     const [data, setdata] = useState([])
     const [likedId, setlikedId] = useState("")
     console.log(likedId)
@@ -14,6 +14,12 @@ const Home = ({setprogress}) => {
     async function main(){
         try {
             setprogress(80)
+            current_d = new Date();
+            contest_d = new Date("2022-04-21T20:00:00")
+
+            if (current_d < contest_d ){
+                throw new Error("Contest has not started yet :) ")
+            }
             let res1 = await axios.get('https://dp1.sytes.net/likedid')
             setlikedId(res1.data.id)
 
